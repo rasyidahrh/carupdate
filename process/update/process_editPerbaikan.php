63d5f16f040714514e19fdb6ac4772f8a129bbb3
rasyidahrh <rasyidahrh84@gmail.com>
2024-08-22 19:08:57 +0800

    naik1

--- process/update/process_editPerbaikan.php
+++ process/update/process_editPerbaikan.php
@@ -8,4 +8,5 @@ $status = $_POST['status'];
 
 $query = mysqli_query($koneksi, "update kerusakan set status='$status' where id='$id'");
 
+$_SESSION['status'] = 'update berhasil tapi foto/file kosogn';
 header("location:" . BASE_URL . "/view/perbaikan/perbaikan.php");
